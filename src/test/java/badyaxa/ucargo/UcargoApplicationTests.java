f54a3cafb8c776ee9fb6986e7056de46ce658112
badyaxa <badyaxa@gmail.com>
2021-05-14 19:18:20 +0300

    Add first entity. Connect h2 database. Connect Heroku Postgres database.

---
+++ src/test/java/badyaxa/ucargo/UcargoApplicationTests.java
@@ -0,0 +1,13 @@
+package badyaxa.ucargo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcargoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
